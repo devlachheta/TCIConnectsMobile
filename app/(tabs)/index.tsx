5ab8fd04df97a813397e52875c5c629e7ddb849d
Sagarchoudharyyy <sagarchoudhary7262@gmail.com>
2026-07-16 16:06:49 +0530

    merge resolved

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -43,15 +43,9 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
-    // alignItems: "center",
-    // justifyContent: "center",
-
-=======
     backgroundColor: "#00254C",
     alignItems: "center",
     justifyContent: "center",
     marginBlock: "auto"
->>>>>>> a056613e18fbee1d48482c01ae0bc9f3d865a36d
   }
 })
