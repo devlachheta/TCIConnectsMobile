a056613e18fbee1d48482c01ae0bc9f3d865a36d
Sagarchoudharyyy <sagarchoudhary7262@gmail.com>
2026-07-16 15:56:07 +0530

    auth input and button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,36 @@
 import AuthFooter from "@/components/authfooter";
+import PrimaryButton from "@/components/PrimaryBotton";
 import { View, Text, StyleSheet } from "react-native";
+import AuthInput from "@/components/authInput";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [password, setPassword] = useState("")
   return (
     <>
+
       <View style={styles.container}>
+        <AuthInput
+          label="Username or Email"
+          placeholder="Email"
+
+          keyboardType="email-address"
+        />
+        <AuthInput
+          label="Password"
+          placeholder="password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <PrimaryButton
+          title="Login"
+          onPress={() => {
+            alert("Login Clicked")
+          }} />
+      </View>
+      <View style={styles.container}>
+
         <AuthFooter />
       </View>
     </>
@@ -14,9 +40,9 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#00254C",
     alignItems: "center",
     justifyContent: "center",
-
+    marginBlock: "auto"
   }
-
 })
---
+++ components/authInput.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Pressable,
+    TextInputProps,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+interface AuthInputProps extends TextInputProps {
+    label: string;
+    error?: string;
+}
+
+export default function AuthInput({
+    label,
+    error,
+    secureTextEntry,
+    ...props
+}: AuthInputProps) {
+    const [hidePassword, setHidePassword] = useState(secureTextEntry);
+    return (
+        <View style={styles.container}>
+            {/* Label */}
+            <Text style={styles.label}>{label}</Text>
+
+            {/* Input */}
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    secureTextEntry={hidePassword}
+                    placeholderTextColor="#94A3B8"
+                    {...props}
+                />
+
+                {secureTextEntry && (
+                    <Pressable
+                        onPress={() => setHidePassword(!hidePassword)}
+                        style={styles.icon}
+                    >
+                        <Ionicons
+                            name={hidePassword ? "eye-off-outline" : "eye-outline"}
+                            size={22}
+                            color="#64748B"
+                        />
+                    </Pressable>
+                )}
+            </View>
+
+            {/* Error */}
+            {error ? <Text style={styles.error}>{error}</Text> : null}
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        marginBottom: 18,
+    },
+
+    label: {
+        fontSize: 15,
+        fontWeight: "600",
+        color: "#1E293B",
+        marginBottom: 8,
+    },
+
+    inputContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "#CBD5E1",
+        borderRadius: 12,
+        backgroundColor: "#FFFFFF",
+        paddingHorizontal: 15,
+    },
+
+    input: {
+        flex: 1,
+        height: 55,
+        fontSize: 16,
+        color: "#1E293B",
+    },
+
+    icon: {
+        paddingLeft: 10,
+    },
+
+    error: {
+        marginTop: 5,
+        color: "#EF4444",
+        fontSize: 13,
+    },
+});
